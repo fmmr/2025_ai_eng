a82ce3106c4226cc51feaae8ddc564a155554e55
Fredrik Rødland <fmr@finn.no>
2025-05-28 12:50:23 +0200

    added test and gha workflow

---
+++ kotlin/src/test/kotlin/com/vend/fmr/aieng/ApplicationTest.kt
@@ -0,0 +1,16 @@
+package com.vend.fmr.aieng
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationTest {
+
+    @Test
+    fun contextLoads() {
+        // This test will pass if the Spring context loads successfully
+        // No assertions needed - context loading failure will fail the test
+    }
+}
