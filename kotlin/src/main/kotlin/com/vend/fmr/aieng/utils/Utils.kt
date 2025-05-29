9079abb971faafaf91e9add791607598c5a03cb9
Fredrik Rødland <fmr@finn.no>
2025-05-29 20:53:05 +0200

    support env varibles if no .env file is found.

--- kotlin/src/main/kotlin/com/vend/fmr/aieng/utils/Utils.kt
+++ kotlin/src/main/kotlin/com/vend/fmr/aieng/utils/Utils.kt
@@ -6,9 +6,9 @@ private val envContent = object {}.javaClass.getResourceAsStream("/.env")
     ?.use { it.readText() }
     ?.split("\n")
     ?.associate { it.substringBefore("=") to it.substringAfter("=") }
-    ?: error(".env file not found in classpath")
 
-fun String.env(): String = envContent[this] ?: System.getenv(this) ?: error("Environment variable '$this' not found")
+
+fun String.env(): String = envContent?.get(this) ?: System.getenv(this) ?: error("Environment variable '$this' not found")
 
 fun String.read(): String = object {}.javaClass.getResourceAsStream(this)
     ?.bufferedReader()
